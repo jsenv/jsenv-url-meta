7869029c46256ab1858a584e388ac22255bb545d
Damien Maillard <damien.maillard@dailymotion.com>
2019-11-12 19:17:50 +0100

    add test to show ** can be used for truly optional char

--- test/applySpecifierPatternMatching/doublewildcard-inside.test.js
+++ test/applySpecifierPatternMatching/doublewildcard-inside.test.js
@@ -13,3 +13,29 @@ import { applySpecifierPatternMatching } from "../../index.js"
   }
   assert({ actual, expected })
 }
+
+{
+  const actual = applySpecifierPatternMatching({
+    specifier: "file:///**.js",
+    url: "file:///a.js",
+  })
+  const expected = {
+    matched: true,
+    index: 12,
+    patternIndex: 13,
+  }
+  assert({ actual, expected })
+}
+
+{
+  const actual = applySpecifierPatternMatching({
+    specifier: "file:///**.js",
+    url: "file:///.js",
+  })
+  const expected = {
+    matched: true,
+    index: 11,
+    patternIndex: 13,
+  }
+  assert({ actual, expected })
+}
